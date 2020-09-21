2490b96010095b849ef8a2c821ec6f553d117720
username <email>
2020-09-21 08:42:31 +0800

    123

--- app/src/main/java/com/mad/trafficclient/fragment/Fragment_1.java
+++ app/src/main/java/com/mad/trafficclient/fragment/Fragment_1.java
@@ -10,6 +10,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -24,7 +26,22 @@ public class Fragment_1 extends Fragment
 				.inflate(R.layout.fragment_layout01, container, false);
 		TextView textview = (TextView) view.findViewById(R.id.textview);
 		textview.setText("this is fragment 1");
+		ListView lv1 = view.findViewById(R.id.lv1);
+		TextView tv1 = view.findViewById(R.id.lv1);
+		Spinner sp1 = view.findViewById(R.id.sp1);
+		initData();
+
+
 		return view;
 	}
+
+	public void initData() {
+
+
+
+
+
+	}
+
 
 }
